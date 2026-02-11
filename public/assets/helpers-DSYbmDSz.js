b7a9a8d5a3a399830d5c353e9a4f30b9efd3db0e
sh4d0wy <sakshambhugra8@gmail.com>
2026-02-11 19:51:27 +0530

    chore: updated UI with client's requests

---
+++ public/assets/helpers-DSYbmDSz.js
@@ -0,0 +1,1 @@
+import{kt as e}from"./index-DqpiM8fm.js";var t=e(`chevron-down`,[[`path`,{d:`m6 9 6 6 6-6`,key:`qrunsl`}]]);function n(e){let t=3600,n=24*t;return e>=n?`${Math.floor(e/n)}d`:e>=t?`${Math.floor(e/t)}h`:e>=60?`${Math.floor(e/60)}m`:`${e}s`}export{t as n,n as t};
